61e7abc0502d74a61c8d4a094613ed66be7fa18a
TanJunHong <e0634975@u.nus.edu>
2020-10-08 01:41:48 +0800

    Add test files

---
+++ src/test/java/mystars/MyStarsTest.java
@@ -0,0 +1,14 @@
+package mystars;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class MyStarsTest {
+    // Three part name for a test: methodUnderTest_inputGiven_expectedOutput
+
+    @Test
+    void sampleTest() {
+        assertTrue(true);
+    }
+}
---
+++ ui-test/test.sh
@@ -0,0 +1,25 @@
+# Create bin directory if it does not exist
+if [ ! -d "../bin" ]
+then
+    mkdir ../bin
+fi
+
+# Compile all source files into bin folder
+if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/mystars/MyStars.java
+then
+    echo "********** Compilation Error **********"
+    exit 1
+fi
+
+# Run the program, feed input and write output
+java -classpath ../bin mystars.MyStars < input.txt > output.txt
+
+# Compare output with expected output (ignoring trailing line separators)
+if diff --strip-trailing-cr output.txt expected.txt
+then
+    echo "Test result: PASSED"
+    exit 0
+else
+    echo "Test result: FAILED"
+    exit 1
+fi
