f2867d7ba79a5a0d8736fd07ebfb44c783a95d75
TanJunHong <b190013@e.ntu.edu.sg>
2020-10-08 01:41:48 +0800

    Add test files

---
+++ ui-test/test.bat
@@ -0,0 +1,17 @@
+@ECHO OFF
+
+REM Create bin directory if it does not exist
+if not exist ..\bin mkdir ..\bin
+
+REM Compile all source files into bin folder
+javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\mystars\MyStars.java
+IF ERRORLEVEL 1 (
+    echo ********** Compilation Error **********
+    exit /b 1
+)
+
+REM Run the program, feed input and write output
+java -classpath ..\bin mystars.MyStars < input.txt > output.txt
+
+REM Compare output with expected output
+FC output.txt expected.txt
