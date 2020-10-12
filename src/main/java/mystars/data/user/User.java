9f8efa8bcd9b487cac18648bf41260c32f400502
TanJunHong <e0634975@u.nus.edu>
2020-10-13 01:05:23 +0800

    Improve user logic

--- src/main/java/mystars/data/user/User.java
+++ src/main/java/mystars/data/user/User.java
@@ -1,4 +1,32 @@
 package mystars.data.user;
 
-public class User {
+public abstract class User {
+
+    private char[] username;
+    private char[] password;
+    private String type;
+
+    public char[] getUsername() {
+        return username;
+    }
+
+    public void setUsername(char[] username) {
+        this.username = username;
+    }
+
+    public char[] getPassword() {
+        return password;
+    }
+
+    public void setPassword(char[] password) {
+        this.password = password;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
