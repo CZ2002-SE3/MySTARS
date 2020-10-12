bfb0e6215ba2cbc63333beabe7a011d4b3db410b
TanJunHong <b190013@e.ntu.edu.sg>
2020-10-13 00:02:13 +0800

    Improve code quality

---
+++ src/main/java/mystars/commands/AddCourseCommand.java
@@ -0,0 +1,20 @@
+package mystars.commands;
+
+import mystars.data.UserList;
+import mystars.storage.Storage;
+import mystars.ui.Ui;
+
+public class AddCourseCommand extends Command {
+
+    /**
+     * Add course to student.
+     *
+     * @param users   UserList object.
+     * @param ui      Ui object.
+     * @param storage Storage object.
+     */
+    @Override
+    public void execute(UserList users, Ui ui, Storage storage) {
+
+    }
+}
--- src/main/java/mystars/data/course/Course.java
+++ src/main/java/mystars/data/course/Course.java
@@ -2,10 +2,10 @@ package mystars.data.course;
 
 public class Course {
 
-    private String courseCode;
-    private String school;
-    private int indexNumber;
-    private int vacancy;
+    private final String courseCode;
+    private final String school;
+    private final int indexNumber;
+    private final int vacancy;
 
     public Course(String courseCode, String school, int indexNumber, int vacancy) {
         this.courseCode = courseCode;
