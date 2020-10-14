570e6bf66b11e2f921c4bb2fb7ac278369058a70
TanJunHong <e0634975@u.nus.edu>
2020-10-14 14:35:12 +0800

    Fix reading of courses

--- src/main/java/mystars/data/CourseList.java
+++ src/main/java/mystars/data/CourseList.java
@@ -1,4 +1,18 @@
 package mystars.data;
 
+import mystars.data.course.Course;
+
+import java.util.ArrayList;
+
 public class CourseList {
+
+    private final ArrayList<Course> courses;
+
+    public CourseList() {
+        courses = new ArrayList<>();
+    }
+
+    public CourseList(ArrayList<Course> courses) {
+        this.courses = courses;
+    }
 }
