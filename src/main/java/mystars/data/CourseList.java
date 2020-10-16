a4e56e0c0e6de9195cabafb79b49e4289dcb4b96
yokemin <woonyokemin@gmail.com>
2020-10-16 23:24:47 +0800

    Add method to calculate total No of AUs for a list of courses

--- src/main/java/mystars/data/CourseList.java
+++ src/main/java/mystars/data/CourseList.java
@@ -15,4 +15,18 @@ public class CourseList {
     public CourseList(ArrayList<Course> courses) {
         this.courses = courses;
     }
+
+    /**
+     * Calculates the total number of AUs for a list of courses.
+     * Used for CourseList defined in Student objects to check for Total AUs restrictions.
+     * @param courses arraylist of courses.
+     * @return total number of AUs of a list of courses.
+     */
+    public int getTotalNoOfAUs(ArrayList<Course> courses) {
+        int totalNoOfAUs = 0;
+        for (Course course: courses) {
+            totalNoOfAUs += course.getNumOfAUs();
+        }
+        return totalNoOfAUs;
+    }
 }
