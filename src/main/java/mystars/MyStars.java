79a693d2163cc43fbadc00e8329376a1bc7ed75f
TanJunHong <b190013@e.ntu.edu.sg>
2020-10-23 01:46:36 +0800

    Add minor improvements

--- src/main/java/mystars/MyStars.java
+++ src/main/java/mystars/MyStars.java
@@ -85,7 +85,7 @@ public class MyStars {
                     ui = new AdminUi();
                     command = parser.parseAdmin(ui.readCommand());
                 }
-
+                ui.showLine();
                 execute(command);
 
             } catch (MyStarsException e) {
